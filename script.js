ead78eb0d9fef7b03aff09ebef4cea1ee03fc971
AxelCoreySIU <axelcorey97@siu.edu>
2017-03-28 17:32:36 -0500

    quote

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function()    {
+    $('div').height("200px");
+    $('div').width("200px");
+    $('div').css("border-radius", "10px");
+    $('div').click(function(){
+      $('div').css("background-color", "red");
+    })
+});
+Contact GitHub API Training Shop Blog About
+
